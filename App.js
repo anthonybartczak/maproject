4140f60d69839b04f0a572526b2da0e2203e4d4d
anthonybartczak <to0oni456@gmail.com>
2020-11-08 14:42:31 +0100

    UI changes

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const getMoviesFromApi = async () => {
+  try {
+    const response = await fetch('https://corona.lmao.ninja/v2/countries/Poland');
+    const json = await response.json();
+    return json.corona;
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.topBar}>
+      <View style={styles.container}>
+        <Text></Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#31326f',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  topBar: {
+    flex: 1,
+    backgroundColor: '#dbf6e9',
   },
 });
