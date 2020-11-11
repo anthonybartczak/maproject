392769614d8870823bbaf5c20dc066aae062ddb7
anthonybartczak <to0oni456@gmail.com>
2020-11-11 18:50:11 +0100

    added API fetching

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-
-const getMoviesFromApi = async () => {
-  try {
-    const response = await fetch('https://corona.lmao.ninja/v2/countries/Poland');
-    const json = await response.json();
-    return json.corona;
-  }
-  catch (error) {
-    console.error(error);
-  }
-};
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator, FlatList} from 'react-native';
 
 export default function App() {
+  
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getStatsFromApiAsync = async () => {
+    try {
+      let response = await fetch(
+        'https://reactnative.dev/movies.json'
+      );
+      let json = await response.json();
+      setData(json.data)
+      setLoading(false)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <ScrollView style={styles.topBar}>
-      <View style={styles.container}>
-        <Text></Text>
-      </View>
+    <ScrollView style={{backgroundColor: '#e7e7de'}}>
+        <TouchableOpacity onPress={getStatsFromApiAsync}>
+          <View style={styles.topBar}>
+            <Text style={styles.topText}>Fetch data</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={{ flex: 1, padding: 24 }}>
+          {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+              data={data}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <Text>{item.title}, {item.releaseYear}</Text>
+              )}
+            />
+          )}
+        </View>
     </ScrollView>
   );
 }
@@ -31,7 +51,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   topBar: {
+    marginTop: 40,
     flex: 1,
-    backgroundColor: '#dbf6e9',
+    backgroundColor: '#0f3057',
   },
+  topText: {
+    textAlign: "center",
+    fontSize: 20,
+    color: '#f4f4f2',
+  }
 });
