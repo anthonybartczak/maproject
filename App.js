38d2df657c2a122046bccd99ff9688de4fc48d7b
anthonybartczak <antoni.bartczak.w@gmail.com>
2020-11-14 20:27:43 +0100

    Final adjustments

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator, FlatList} from 'react-native';
-import { PieChart } from 'react-minimal-pie-chart';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator, Dimensions} from 'react-native';
+import { PieChart } from "react-native-chart-kit";
+
 
 export default function App() {
   
@@ -23,44 +24,87 @@ export default function App() {
     }
   };
 
+  const screenWidth = Dimensions.get("window").width;
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false,
+    propsForLabels: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'bold',
+    },
+  };
+
+  const PCdata = [
+    {
+      name: "cases",
+      population: data.todayCases,
+      color: "#E64D3E",
+      legendFontColor: "#040c16",
+      legendFontSize: 15
+    },
+    {
+      name: "recovered",
+      population: data.todayRecovered,
+      color: "#C1E610",
+      legendFontColor: "#040c16",
+      legendFontSize: 15
+    },
+    {
+      name: "deaths",
+      population: data.todayDeaths,
+      color: "#277DE6",
+      legendFontColor: "#040c16",
+      legendFontSize: 15
+    },
+  ];
+
   return (
     <ScrollView style={{backgroundColor: '#e7e7de'}}>
-      
+
         <TouchableOpacity onPress={fetchCoronaData}>
           <View style={styles.topBar}>
             <Text style={styles.topText}>Fetch data</Text>
           </View>
         </TouchableOpacity>
 
-        <View style={{ flex: 1, padding: 24 }}>
+        <View>
           {isLoading ? <ActivityIndicator/> : (
             <View>
-              <View>
-                <Text styles={styles.dataStyles}>Cases today: {data.todayCases}</Text>
-                <Text styles={styles.dataStyles}>Cases total: {data.cases}</Text>
-                <Text> </Text>
-                <Text styles={styles.dataStyles}>Deaths today: {data.todayDeaths}</Text>
-                <Text styles={styles.dataStyles}>Deaths total: {data.deaths}</Text>
+              <View style={styles.dataDesign}>
+                <Text style={styles.dataStyles}>{data.todayCases}</Text>
+                <Text style={styles.labelStyles}>cases today</Text>
+
+                <Text style={styles.dataStyles}>{data.cases}</Text>
+                <Text style={styles.labelStyles}>total cases</Text>
+
+                <Text style={styles.dataStyles}>{data.todayDeaths}</Text>
+                <Text style={styles.labelStyles}>deaths today</Text>
+
+                <Text style={styles.dataStyles}>{data.deaths}</Text>
+                <Text style={styles.labelStyles}>total deaths</Text>
               </View>
               <PieChart
-                label={({ dataEntry }) => dataEntry.value}
-                labelStyle={(index) => ({
-                  fontSize: '5px',
-                  fontFamily: 'sans-serif',
-                })}
-                labelPosition={60}
-                animate
-                radius={30}
-                lineWidth={15}
-                paddingAngle={5}
-                data={[
-                  { title: 'One', value: data.todayCases, color: '#E38627' },
-                  { title: 'Two', value: data.todayRecovered, color: '#C13C37' },
-                ]}
+                data={PCdata}
+                width={screenWidth}
+                height={220}
+                chartConfig={chartConfig}
+                accessor="population"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+                hideLegend='true'
               />
             </View>
           )}
         </View>
+
     </ScrollView>
   );
 }
@@ -72,19 +116,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dataDesign: {
+    marginBottom: 70,
+  },
   topBar: {
-    marginTop: 40,
     flex: 1,
     backgroundColor: '#0f3057',
+    marginBottom: 40,
+    marginRight:20,
+    marginLeft:20,
+    marginTop:10,
+    paddingTop:20,
+    paddingBottom:20,
+    borderRadius:10,
+    borderWidth: 1,
   },
   topText: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: 30,
     color: '#f4f4f2',
   },
   dataStyles: {
     textAlign: "center",
+    fontSize: 25,
+    fontWeight: 'bold',
     fontFamily: 'sans-serif',
-    fontSize: '8px'
+    color: '#0f3057',
   },
+  labelStyles: {
+    textAlign: "center",
+    fontSize: 19,
+    fontFamily: 'sans-serif',
+    marginBottom: 15,
+  },
+  circleOrange: {
+    width: 25,
+    height: 25,
+    borderRadius: 100/2,
+    backgroundColor: '#E38627'
+  },
+  circleRed: {
+    width: 25,
+    height: 25,
+    borderRadius: 100/2,
+    backgroundColor: '#C13C37'
+}
 });
